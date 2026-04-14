049341fb44a9926906932044bb4625fb929ed85c
opilane <opilane@HP-CZC2349HTD>
2026-04-14 09:53:12 +0300

    .

---
+++ OOP/OOP/Isik.cs
@@ -0,0 +1,40 @@
+﻿namespace OOP
+{
+    public class Isik
+    {
+        // Omadused ehk väljad
+        // Privaatne väli - otse ligi ei saa
+        private int sünniaasta;
+
+        // Avalik omadus (Property) automaatse get/set logikaga
+        public string Nimi { get; set; }
+
+        // Kontrollitud omadus
+        public int Sünniaasta
+        {
+            get { return sünniaasta; }
+            set
+            {
+                if (value > 1900 && value <= DateTime.Now.Year)
+                    sünniaasta = value;
+                else
+                    Console.WriteLine("Vigane sünniaasta!");
+            }
+        }
+
+        // Arvutatud omadus (ainult lugemiseks / getter)
+        public int Vanus => DateTime.Now.Year - sünniaasta;
+
+        // Tegevus ehk meetod
+        public void Tervita()
+        {
+            if (string.IsNullOrEmpty(Nimi) || sünniaasta == 0)
+            {
+                Console.WriteLine("Vigane sünniaasta!");
+            }
+            else
+                Console.WriteLine($"Tere! Mina olen {Nimi} ja ma olen {Vanus} aastat vana. Olen sündinud {Sünniaasta}");
+            
+        }
+    }
+}
---
+++ OOP/OOP/StartPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    internal class StartPage
+    {
+        public static void Main(string[] args)
+        {
+            Isik inimene1 = new Isik();
+            inimene1.Nimi = "Mati";
+            inimene1.Sünniaasta = 28;
+            inimene1.Tervita();
+            // Väljund: Tere! Mina olen Mati...
+        }
+    }
+}
